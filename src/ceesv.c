3513c47369fdff51bdee3e206aff56de462f9e9b
Josh Whetton <whetton.josh@gmail.com>
2019-03-31 10:51:16 -0400

    Splits csv strings properly.
    Stopped tracking the binary artifact...

--- src/ceesv.c
+++ src/ceesv.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 255
 
+
+int csvparse(char buf[]);
 
 int main()
 {
     FILE *fp;
     char buff[MAX];
+    char *token;
+    const char delim[2] = ",";
 
+	// Open the file
     fp = fopen("./testing.csv", "r");
-    while(fgets(buff, MAX, (FILE*)fp))
-        printf("Line: %s", buff);
 
+	// Read each line in file
+    while(fgets(buff, MAX, (FILE*)fp))
+    {
+        // Print line
+        printf("Line:\n\t%s", buff);
+        token = strtok(buff, delim);
+
+		printf("Tokens:\n\t");
+        while (token != NULL) {
+            printf("%s ", token);
+            token = strtok(NULL, delim);
+        }
+        printf("\n");
+    }
 }
