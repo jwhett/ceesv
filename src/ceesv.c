c791c37cd49aeb0e63a3b546753053355d9d2a32
Josh Whetton <whetton.josh@gmail.com>
2019-03-31 17:37:39 -0400

    moved to function

--- src/ceesv.c
+++ src/ceesv.c
@@ -2,18 +2,25 @@
 #include <string.h>
 #define MAX 255
 
+void printcsv(FILE *f);
+
 int main()
 {
     FILE *fp;
+
+    // Open the file
+    fp = fopen("./testing.csv", "r");
+	printcsv(fp);
+}
+
+void printcsv(FILE *f)
+{
     char buff[MAX];
     char *token;
     const char delim[2] = ",";
 
-    // Open the file
-    fp = fopen("./testing.csv", "r");
-
     // Read each line in file
-    while(fgets(buff, MAX, (FILE*)fp))
+    while(fgets(buff, MAX, (FILE*)f))
     {
         // Print line
         printf("Line:\n\t%s", buff);
